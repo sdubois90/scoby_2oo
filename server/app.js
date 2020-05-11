df0ad5b98ddb1c8c73d7bad8d8f87b7f4df69beb
sdubois92 <seb.dubois15@free.fr>
2020-05-11 15:36:34 +0200

    map

--- server/app.js
+++ server/app.js
@@ -9,10 +9,16 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const app = express();
+const cors = require('cors')
 
 /**
  * Middlewares
  */
+app.use(cors({
+  origin: process.env.FRONT_END_URL,
+})
+);
+
 
 app.use(logger("dev")); // This logs HTTP reponses in the console.
 app.use(express.json()); // Access data sent as json @req.body
@@ -34,6 +40,7 @@ app.use(function (req, res, next) {
   console.log(req.session.currentUser);
   next();
 });
+
 
 /**
  * Routes
