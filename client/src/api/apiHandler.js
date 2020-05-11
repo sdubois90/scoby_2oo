b5f59bb22f872cd029c866c2cd6ca2e8d9cd8299
Maxime Paolone <paolonemax@gmail.com>
2020-05-11 17:50:43 +0200

    Mapbox

--- client/src/api/apiHandler.js
+++ client/src/api/apiHandler.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const service = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true, // Cookie is sent to client when using this service. (used for session)
+  // withCredentials: true, // Cookie is sent to client when using this service. (used for session)
 });
 
 function errorHandler(error) {
